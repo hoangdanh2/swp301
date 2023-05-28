0c016d362b552544f9992f0b2bcd08ba35a1405f
DANH <danhthse173598@fpt.edu.vn>
2023-05-28 16:37:10 +0700

    login

---
+++ src/pages/contact/contact.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./contact.css"
+export const Contact = () => {
+    return (
+        <div className="contact">
+        <h1 className="title-contact">Contact</h1>
+        <div className="form-contact">
+        <form>
+            <label>Name: </label>
+            <input type="text" id="name" required />
+
+            <label>Email: </label>
+            <input type="email" id="email" required />
+
+            <label>Message: </label>
+            <input type="message" id="message" required />
+
+            <input type="submit" value="Submit"/>
+        </form>
+        </div>
+        </div>
+        
+        
+    );
+};
